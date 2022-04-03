c72e261ed0c7c0d7f11a4b6f3f96acf208247d5e
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-03 23:26:01 +0300

    adding icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,15 @@
 import "./sidebar.scss"
+import DashboardIcon from "@mui/icons-material/Dashboard"
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined"
+import PaymentIcon from "@mui/icons-material/Payment"
+import LocalShippingIcon from "@mui/icons-material/LocalShipping"
+import QueryStatsIcon from "@mui/icons-material/QueryStats"
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone"
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream"
+import PsychologyIcon from "@mui/icons-material/Psychology"
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+import LogoutIcon from "@mui/icons-material/Logout"
+import InventoryIcon from "@mui/icons-material/Inventory"
 
 function Sidebar() {
   return (
@@ -10,16 +21,48 @@ function Sidebar() {
       <div className="center">
         <ul>
           <li>
+            <DashboardIcon />
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineOutlinedIcon />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <InventoryIcon />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <PaymentIcon />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <QueryStatsIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <SettingsSystemDaydreamIcon />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <AccountCircleIcon />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
