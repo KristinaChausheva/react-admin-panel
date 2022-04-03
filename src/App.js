5f6659f3e8145e10e43d86274818dffc980d6cc3
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-03 22:07:03 +0300

    addign routes to the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Home from "./pages/home/Home"
+import List from "./pages/list/List"
+import Login from "./pages/login/Login"
+import New from "./pages/new/New"
+import Single from "./pages/single/Single"
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />}>
+            <Route path="users"></Route>
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
