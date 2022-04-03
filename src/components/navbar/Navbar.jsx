0661dddd1b205394779e88db74ee91ff3ef4fd50
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-04 00:29:07 +0300

    nvbar icons added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,46 @@
-import "./havbar.scss"
+import "./navbar.scss"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined"
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined"
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined"
+import ListAltOutlinedIcon from "@mui/icons-material/ListAltOutlined"
 
 function Navbar() {
   return (
-    <div className="home">
-      <h1 className="title">Navbar Page</h1>
+    <div className="sidebar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+      </div>
+      <div className="items">
+        <div className="item">
+          <LanguageOutlinedIcon />
+          English
+        </div>
+        <div className="item">
+          <DarkModeOutlinedIcon />
+          English
+        </div>
+        <div className="item">
+          <NotificationsNoneOutlinedIcon />
+          English
+        </div>
+        <div className="item">
+          <DarkModeOutlinedIcon />
+          English
+        </div>
+        <div className="item">
+          <ChatBubbleOutlineOutlinedIcon />
+          English
+        </div>
+        <div className="item">
+          <ListAltOutlinedIcon />
+          English
+        </div>
+      </div>
     </div>
   )
 }
