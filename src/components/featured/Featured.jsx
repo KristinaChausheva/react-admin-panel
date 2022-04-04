66c81d3223de2262a400e5935dda19d11a0f407c
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-05 02:13:37 +0300

    styling featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,7 @@
 import "./featured.scss"
 import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined"
+import { CircularProgressbar } from "react-circular-progressbar"
+import "react-circular-progressbar/dist/styles.css"
 
 function Featured() {
   return (
@@ -7,6 +9,16 @@ function Featured() {
       <div className="top">
         <h1 className="title">Total Revenue</h1>
         <MoreVertOutlinedIcon />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amout">$420</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be included.
+        </p>
       </div>
     </div>
   )
