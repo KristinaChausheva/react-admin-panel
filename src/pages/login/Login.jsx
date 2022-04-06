1ab249eadefd3cab63682b08aa5ef6d5049f856e
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-06 19:39:40 +0300

    login implementation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,42 @@
 import "./login.scss"
-
+import { useState } from "react"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../../firebase"
 function Login() {
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        setError(true)
+        // ..
+      })
+  }
   return (
     <div className="login">
-      <form action="">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
         <span>Wrong email or password</span>
       </form>
